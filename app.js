4e879c1c3ac6015114e3959a886b137c6e93159a
karthik182000 <75200967+karthik182000@users.noreply.github.com>
2024-04-23 22:59:13 +0530

    Ignition completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //For nested div tags 
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const div=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"Hi there")));
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(div)
